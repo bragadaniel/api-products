6cb6ae81f5dea91c9e3eaa0f28b2b72212e041dd
Daniel B <braga.o.daniel@gmail.com>
2023-11-08 19:37:06 -0300

    feat: install validation marshmallow and configs

--- main.py
+++ main.py
@@ -1,5 +1,36 @@
-from flask import Flask
+from typing import Any
+from flask import Flask, jsonify, make_response, request
+from marshmallow import ValidationError
+from bd import Cars
+from schema import CarSchema
 
 app = Flask(__name__)
 
-app.run()
+@app.route('/cars', methods=['GET'])
+def get_cars():
+  return make_response(jsonify(
+    message= 'Cars List',
+    value=Cars
+    ))
+
+@app.route('/cars', methods=['POST'])
+def create_car():
+  try:
+     data: Any = request.json
+     schema = CarSchema()
+     result: Any = schema.load(data)
+  except ValidationError as err:
+     return bad_request(err.messages)
+      
+  Cars.append(result)
+  return  make_response(jsonify(
+    message='Created success'
+  ), 201)
+
+@app.errorhandler(400)
+def bad_request(e):
+    return make_response(jsonify(
+    message=e
+  ), 400)
+
+app.run(port=8000)
---
+++ schema.py
@@ -0,0 +1,7 @@
+from marshmallow import Schema, fields
+
+class CarSchema(Schema):
+    make = fields.Str(required=True)
+    year = fields.Integer(required=True)
+    model = fields.Str()
+    id = fields.Str()
